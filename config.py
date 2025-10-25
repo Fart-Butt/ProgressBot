d70410a65fa4e07ef8856a645a2975b16629ad8b
Fart-Butt <36031808+Fart-Butt@users.noreply.github.com>
2025-10-25 15:14:31 -0400

    first post conversion to env file

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+secretkey = os.environ['progressbot_discordapikey']  # this one for the real chat room
+
+test_environment = os.environ['progressbot_test_environment']
+command_prefix = os.environ['progressbot_command_prefix']
+
+master_config = "test"  # used for timezone calculations
+
+db_secrets = [os.environ['progressbot_db_username'], os.environ['progressbot_db_password']] # db config for storage for vacuum.py
+server= {
+    'ip':os.environ['progressbot_server_ip'],
+    'password': os.environ['progressbot_server_password'],
+}
+
+minecraft_db = os.environ['progressbot_minecraft_db']
+
+global_ignore_list = os.environ['progressbot_global_ignore_list']
