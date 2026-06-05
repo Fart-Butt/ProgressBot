50d7bacf399c31d695c207155cf357ea9d28cc37
Fart-Butt <36031808+Fart-Butt@users.noreply.github.com>
2026-06-05 13:23:13 -0400

    generalized mysql connection config

--- config.py
+++ config.py
@@ -4,16 +4,11 @@ secretkey = os.environ['progressbot_discordapikey']  # this one for the real cha
 test_environment = os.environ['progressbot_test_environment']
 command_prefix = os.environ['progressbot_command_prefix']
 
-master_config = "test"  # used for timezone calculations
-
-db_secrets = [os.environ['progressbot_db_username'], os.environ['progressbot_db_password']] # db config for storage for vacuum.py
-server= {
-    'ip':os.environ['progressbot_server_ip'],
-    'password': os.environ['progressbot_server_password'],
-}
-
-minecraft_db = os.environ['progressbot_minecraft_db']
-
+db_secrets = {
+    'username': os.environ['progressbot_db_username'],
+    'password': os.environ['progressbot_db_password'],
+    'port': os.environ['progressbot_db_port'],
+    'host': os.environ['progressbot_db_host'],
+    'database': os.environ['progressbot_db_database']
+} # db config
 global_ignore_list = os.environ['progressbot_global_ignore_list']
-
-mc_scripts_path = os.environ['progressbot_mc_base_path'] + "scripts/"
