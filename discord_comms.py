d9bbf362071132b063ba350348393813deb2593c
Fart-Butt <36031808+Fart-Butt@users.noreply.github.com>
2026-06-05 13:07:45 -0400

    Refactor away from buttbot

--- discord_comms.py
+++ discord_comms.py
@@ -15,9 +15,3 @@ class DiscordComms:
             await asyncio.sleep(rand.randint(2, 5))
             msg = await channel.send(message)  # dont remove await from here or this shit will break
             return msg
-
-    async def do_security_log(self, message: str):
-        await self.do_send_message(shared.bot.get_channel(505226379487346690), message)
-
-    async def do_info_log(self, message: str):
-        await self.do_send_message(shared.bot.get_channel(505226325511110658), message)
